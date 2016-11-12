a10110da07bded921abe0181faf813c79ee1985a
Emmanuel Pilande <epilande@gmail.com>
2016-11-12 00:27:51 -1000

    Allow user to select project directory

--- app/src/containers/App/App.js
+++ app/src/containers/App/App.js
@@ -1,20 +1,34 @@
 import React, { Component } from 'react';
+import { remote } from 'electron';
+
 import styles from './App.css';
 
 export default class App extends Component {
-  static propTypes = {
-  };
+  constructor(props) {
+    super(props);
+    this.setProjectDir = this.setProjectDir.bind(this);
+    this.state = {
+      project: '',
+    };
+  }
+
+  setProjectDir() {
+    const { dialog } = remote;
+    const dir = dialog.showOpenDialog({ properties: ['openDirectory'] });
+
+    if (dir) {
+      const [head] = dir;
+      this.setState({ project: head });
+    }
+  }
 
   render() {
     return (
       <div className={styles.base}>
         <div className={styles.trayTriangle} />
         <main className={styles.main}>
-          Select Project:
-          <select>
-            <option>123</option>
-            <option>abc</option>
-          </select>
+          <button onClick={this.setProjectDir}>Select Project Folder</button>
+          <p>{this.state.project}</p>
         </main>
       </div>
     );
