f3ea2d92e3cc6a238d0151d632eee1ee2be8c747
Emmanuel Pilande <epilande@gmail.com>
2016-11-12 02:29:15 -1000

    💇 Clean up unused code

--- app/main.js
+++ app/main.js
@@ -3,42 +3,7 @@ import electron, { ipcMain } from 'electron';
 
 import runMocha from './lib/start';
 
-// Module to control application life.
 const app = electron.app;
-// Module to create native browser window.
-const BrowserWindow = electron.BrowserWindow;
-
-// Keep a global reference of the window object, if you don't, the window will
-// be closed automatically when the JavaScript object is garbage collected.
-let mainWindow;
-
-function createWindow() {
-  // Create the browser window.
-  mainWindow = new BrowserWindow({
-    width: 800,
-    height: 600,
-  });
-
-  // and load the index.html of the app.
-  mainWindow.loadURL(`file://${__dirname}/index.html`);
-
-  mainWindow.on('ready-to-show', () => {
-    // Open the DevTools.
-    mainWindow.webContents.openDevTools();
-  });
-
-  // Emitted when the window is closed.
-  mainWindow.on('closed', () => {
-    // Dereference the window object, usually you would store windows
-    // in an array if your app supports multi windows, this is the time
-    // when you should delete the corresponding element.
-    mainWindow = null;
-  });
-}
-
-// This method will be called when Electron has finished
-// initialization and is ready to create browser windows.
-// app.on('ready', createWindow);
 
 // Quit when all windows are closed.
 app.on('window-all-closed', () => {
@@ -46,14 +11,6 @@ app.on('window-all-closed', () => {
   // to stay active until the user quits explicitly with Cmd + Q
   if (process.platform !== 'darwin') {
     app.quit();
-  }
-});
-
-app.on('activate', () => {
-  // On OS X it's common to re-create a window in the app when the
-  // dock icon is clicked and there are no other windows open.
-  if (mainWindow === null) {
-    createWindow();
   }
 });
 
@@ -65,18 +22,18 @@ const mb = menubar({
   resizable: false,
   transparent: true,
 });
+
 mb.on('ready', () => {
   console.log('app is ready'); // eslint-disable-line
   // your app code here
 });
 
-mb.on('after-create-window', function () {
+mb.on('after-create-window', () => {
   // ====================================================================
   // This is where the data gets passed to `src/index.js`,
   // Move to a function call triggered by the frontend.
   // ====================================================================
   mb.window.webContents.on('dom-ready', () => {
-
     ipcMain.on('execute test', (event, path) => {
       runMocha(path, (err, data) => {
         if (err) {
@@ -85,7 +42,6 @@ mb.on('after-create-window', function () {
         return mb.window.webContents.send('test results', data);
       });
     });
-
   });
   // ====================================================================
 });
