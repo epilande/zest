b42cf02113b2b49354b3d66038febf9d872961f2
Tony Gaskell <thgaskell@gmail.com>
2016-11-11 18:53:03 -1000

    Add developer tools

--- app/main.js
+++ app/main.js
@@ -17,8 +17,10 @@ function createWindow () {
   // and load the index.html of the app.
   mainWindow.loadURL('file://' + __dirname + '/index.html');
 
-  // Open the DevTools.
-  mainWindow.webContents.openDevTools();
+  mainWindow.on('after-create-window', function () {
+    // Open the DevTools.
+    mainWindow.webContents.openDevTools();
+  });
 
   // Emitted when the window is closed.
   mainWindow.on('closed', function() {
