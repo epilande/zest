dcd304f64ba642ada2411cea8fc9f32febbb055a
Tony Gaskell <thgaskell@gmail.com>
2016-11-12 16:43:11 -1000

    report grep karma + mocha results to app

--- app/lib/start.js
+++ app/lib/start.js
@@ -1,14 +1,5 @@
-import Mocha from 'mocha';
 import path from 'path';
-
-/**
- * Resets the suite
- * @param {Suite} suite the suit.
- */
-function resetTests(suite) {
-  suite.tests = [];
-  suite.suites = [];
-}
+import childProcess from 'child_process';
 
 /**
  * Resolves the absolute file path.
@@ -20,36 +11,51 @@ function resolvePath(relativePath) {
 }
 
 /**
+ * [handleNpmTest description]
+ * @param  {[type]} err    [description]
+ * @param  {[type]} stdout [description]
+ * @param  {[type]} stderr [description]
+ * @return {[type]}        [description]
+ */
+function handleNpmTest(err, stdout, stderr, callback) {
+  if (err &&
+      stderr &&
+      !stderr.match(/Test failed/)
+  ) {
+    return callback(err);
+  }
+  // Report the test results;
+  let testResults;
+  if (stdout.match(/karma/i)) {
+    try { // Karma parser
+      testResults = JSON.parse(stdout.slice(stdout.indexOf('{')));
+    } catch (e) {
+      return callback('Could not parse test results, use the `karma-json-reporter` module.');
+    }
+  } else {
+    try { // Mocha parser
+      testResults = JSON.parse(stdout.slice(stdout.indexOf('{')));
+    } catch (e) {
+      return callback('Could not parse test results, use a mocha `json` reporter.');
+    }
+  }
+  return callback(null, testResults);
+}
+
+/**
  * Runs mocha on a given path
  * @param  {String}   _projectPath  The path the mocha project
  * @param  {Function} callback      The callback handler
  */
 export default function (_projectPath, callback) {
-  let filePaths;
+  let projectPath;
   try {
-    filePaths = Mocha.utils
-      .files(resolvePath(_projectPath))
-      .map(resolvePath);
+    projectPath = resolvePath(_projectPath);
   } catch (err) {
     return callback(err);
   }
-  const mocha = new Mocha();
-  mocha.reporter('json');
 
-  if (filePaths.length > 0) {
-    filePaths.forEach(filepath => delete require.cache[filepath]);
-  }
-
-  filePaths.forEach(filepath => mocha.addFile(filepath));
-
-  const runner = mocha.run((/* failures */) => {
-    const results = runner.testResults;
-
-    mocha.files = [];
-    if (mocha.options) {
-      mocha.options.files = [];
-    }
-    resetTests(mocha.suite);
-    return callback(null, results);
-  });
+  return childProcess.exec('npm test', {
+    cwd: projectPath,
+  }, (err, stdout, stderr) => handleNpmTest(err, stdout, stderr, callback));
 }
