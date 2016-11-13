0d0ba55bdba6357fe71cdaa27e21c798e0dfe543
Emmanuel Pilande <epilande@gmail.com>
2016-11-12 22:46:18 -1000

    Set up Selection & Project routes

--- app/src/routes.js
+++ app/src/routes.js
@@ -1,8 +1,12 @@
 import React from 'react';
 import { Route, IndexRedirect } from 'react-router';
 import App from 'containers/App';
+import Selection from 'containers/Selection';
+import Project from 'containers/Project';
 
 export default (
   <Route path="/" component={App}>
+    <Route path="selection" component={Selection} />
+    <Route path="project" component={Project} />
   </Route>
 );
