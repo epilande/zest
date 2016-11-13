98f839984a7cc0590b7524ec6844f1d215356fb7
Emmanuel Pilande <epilande@gmail.com>
2016-11-12 20:26:36 -1000

    Import styles into index.js

--- app/src/index.js
+++ app/src/index.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import { render } from 'react-dom';
 import { Provider } from 'react-redux';
-import 'sanitize.css/sanitize.css';
 import App from 'containers/App';
 import configureStore from './store';
+
+import './styles/app.css';
 
 const store = configureStore();
 
