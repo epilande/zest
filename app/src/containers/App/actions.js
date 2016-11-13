2dd9dbffb71472e6b5f8b677899f675f1b8abff6
Tony Gaskell <thgaskell@gmail.com>
2016-11-13 08:28:58 -1000

    Add progress status to tests

--- app/src/containers/App/actions.js
+++ app/src/containers/App/actions.js
@@ -1,6 +1,7 @@
 import {
   SET_PROJECTS,
   UPDATE_PROJECT,
+  UPDATE_PROJECT_PROGRESS,
 } from './constants';
 
 export function setProjects(projects) {
@@ -14,6 +15,14 @@ export function updateProject(projectPath, results) {
   return {
     type: UPDATE_PROJECT,
     projectPath,
-    ...results,
+    results,
+  };
+}
+
+export function updateProjectProgress(projectPath, inProgress) {
+  return {
+    type: UPDATE_PROJECT_PROGRESS,
+    projectPath,
+    inProgress,
   };
 }
