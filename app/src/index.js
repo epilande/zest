41a690880629095b36d038e2583dac1aa19e8d6c
Emmanuel Pilande <epilande@gmail.com>
2016-11-12 22:34:52 -1000

    Add react-router

--- app/src/index.js
+++ app/src/index.js
@@ -1,15 +1,16 @@
 import React from 'react';
 import { render } from 'react-dom';
 import { Provider } from 'react-redux';
+import { Router, hashHistory } from 'react-router';
 import 'sanitize.css/sanitize.css';
-import App from 'containers/App';
+import routes from './routes';
 import configureStore from './store';
 
 const store = configureStore();
 
 render(
   <Provider store={store}>
-    <App />
+    <Router history={hashHistory} routes={routes} />
   </Provider>,
   document.getElementById('root'),
 );
