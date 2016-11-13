4265a796a80868ee8a8311596def38909dc76f96
Tony Gaskell <thgaskell@gmail.com>
2016-11-12 20:47:00 -1000

    Add default watch time of 1 second

--- app/lib/watch.js
+++ app/lib/watch.js
@@ -1,8 +1,10 @@
 import watch from 'node-watch';
 import debounce from 'lodash-debounce';
 
+const DEFAULT_DEBOUNCE_RATE = 1 * 1000;  // 1 second
+
 function createPathWatcher(path, options = {}, callback) {
-  const { DEBOUNCE_RATE } = options;
+  const DEBOUNCE_RATE = options.DEBOUNCE_RATE || DEFAULT_DEBOUNCE_RATE;
 
   const watchHandler = (/* filename */) => debounce(callback.bind(null, path), DEBOUNCE_RATE);
   const watcher = watch(path, options, watchHandler);
